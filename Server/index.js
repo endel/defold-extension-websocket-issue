915ad4ab072d6295ab1d140b8f966770783d350d
Endel Dreyer <endel.dreyer@gmail.com>
2020-09-26 18:35:08 -0300

    add more examples for server-initiated disconnection

--- Server/index.js
+++ Server/index.js
@@ -5,8 +5,25 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', function connection(ws) {
   console.log("client connected!");
 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', function incoming(data) {
+    var message = data.toString();
+
+    if (message === "close") {
+      //
+      // Close with code 1005 (CLOSED_NO_STATUS)
+      // (See all WebSocket close codes here: https://github.com/Luka967/websocket-close-codes)
+      //
+      console.log("Close with code 1005 (CLOSED_NO_STATUS)");
+      ws.close();
+
+    } else if (message === "terminate") {
+      //
+      // Close with code 1006 (CLOSE_ABNORMAL)
+      // (See all WebSocket close codes here: https://github.com/Luka967/websocket-close-codes)
+      //
+      console.log("Close with code 1006 (CLOSE_ABNORMAL)");
+      ws.terminate()
+    }
   });
 
   //
